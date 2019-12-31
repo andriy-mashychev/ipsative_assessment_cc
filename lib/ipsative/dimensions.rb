67cdb156377e5b16c295fe8b34ad62daf19b8f52
Andriy Mashychev <andriy.mashychev@live.com>
2019-12-31 13:07:43 +0100

    Load pre-defined answers from the JSON file.

--- lib/ipsative/dimensions.rb
+++ lib/ipsative/dimensions.rb
@@ -1,6 +1,12 @@
+require "json"
 require "ostruct"
 
 module Ipsative
   class Dimension < OpenStruct
   end
+
+  DIMENSIONS = JSON.parse(
+    File.read(File.join(__dir__, '../../data/dimensions.json')),
+    object_class: Dimension
+  ).map! { |dimension_obj| dimension_obj.freeze }.freeze
 end
